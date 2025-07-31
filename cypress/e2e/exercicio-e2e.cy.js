c2f881db19d653c8a714987a0d0a1934372cb078
cleberdevfull <cleber.santos.almeida@gmail.com>
2025-07-31 18:41:47 -0300

    Finalizando automação e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -27,63 +27,62 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     beforeEach(() => {
-            cy.session('Login', () => {
+        cy.session('Login', () => {
             cy.visit('minha-conta')
             cy.login(dadosLogin.usuario, dadosLogin.senha)
             cy.get('.page-title').should('contain', 'Minha conta')
         })
-          
-            cy.visit('produtos');
-           
 
-        });
-         // Validando o login 
-            it('Confirma login com sucesso', () => {
-            cy.visit('/minha-conta');
-            cy.get('.page-title').should('contain', 'Minha conta');
-             });
-/*
-        afterEach(() => {
-        cy.screenshot()
-        });
- */ 
-       // Iniciando o fluxo de busca do produto, seleção, adição ao carrinho, checkout e validação do pedido
-        describe('Adicionando produtos ao carrinho e efetuando checkout', () => {
-           
-            it('Buscar produto', () => {
-            ProdutosPage.buscarProduto('Typhon Performance Fleece-lined Jacket');
-            cy.get('.product_title').should('contain','Typhon Performance Fleece-lined Jacket');
+        cy.visit('produtos');
+
+
+    });
+    // Validando o login 
+    it('Confirma login com sucesso', () => {
+        cy.visit('/minha-conta');
+        cy.get('.page-title').should('contain', 'Minha conta');
+    });
+    /*
+            afterEach(() => {
+            cy.screenshot()
             });
-            
-            it('Adicionar produto ao carrinho', () => {
+     */
+
+    // Iniciando o fluxo de busca do produto, seleção, adição ao carrinho, checkout e validação do pedido
+    describe('Adicionando produtos ao carrinho e efetuando checkout', () => {
+
+        it('Buscar produto', () => {
+            ProdutosPage.buscarProduto('Typhon Performance Fleece-lined Jacket');
+            cy.get('.product_title').should('contain', 'Typhon Performance Fleece-lined Jacket');
+        });
+
+        it('Adicionar produto ao carrinho', () => {
             ProdutosPage.buscarProduto('Typhon Performance Fleece-lined Jacket');
             ProdutosPage.addProdutoCarrinho();
-            cy.get('.woocommerce-message').should('contain' , ' foram adicionados no seu carrinho.');
-            });
+            cy.get('.woocommerce-message').should('contain', ' foram adicionados no seu carrinho.');
+        });
 
-            it('Acessando checkout', () => {
+        it('Acessando checkout', () => {
             CheckoutPage.acessarCheckout()
-            cy.get('.breadcrumb > .active').should('contain' , 'Checkout')
-            });
+            cy.get('.breadcrumb > .active').should('contain', 'Checkout')
+        });
 
-            it('Preenchendo detalhes do faturamento', () => {
+        it('Preenchendo detalhes do faturamento', () => {
             CheckoutPage.acessarCheckout()
             CheckoutPage.detalhesFaturamento()
             cy.get('.product-name').eq(3).should('contain', 'Typhon Performance Fleece-lined Jacket')
-            });
+        });
 
-            it('Selecionando método de pagamento', () => {
+        it('Selecionando método de pagamento', () => {
             CheckoutPage.acessarCheckout()
             CheckoutPage.selecionarMetodoPagamento()
-            });
-    
-        /*
-            it('Validando pedido', () => {
-            CheckoutPage.acessarPedido()
-            CheckoutPage.validarPedido()
-            });
-        */
-         });
+        });
+
+        it('Validando a compra', () => {
+            CheckoutPage.validandoCompra();
+        });
 
     });
-             
+
+});
+
--- cypress/support/page_objects/checkout.page.js
+++ cypress/support/page_objects/checkout.page.js
@@ -1,14 +1,14 @@
 
-class Checkout{
-        
+class Checkout {
 
-        acessarCheckout(checkout) {
+
+    acessarCheckout(checkout) {
         cy.get('.dropdown-toggle > .text-skin').click()
         cy.get('.button.checkout.wc-forward').eq(1).click()
 
-        }
+    }
 
-        detalhesFaturamento(detalhes) {
+    detalhesFaturamento(detalhes) {
         cy.get('#billing_company').clear().type('Escola Britanica de Artes e Comércio')
         cy.get('#select2-billing_country-container').click()
         cy.get('.select2-results__option').contains('Brasil').click()
@@ -20,28 +20,37 @@ class Checkout{
         cy.get('#billing_postcode').clear().type('05417-000')
         cy.get('#billing_phone').clear().type('11999999999')
         cy.get('#order_comments').clear().type('Gostaria de receber o pedido no período da manhã')
-        } 
+    }
 
-        selecionarMetodoPagamento(metodoPagamento) {
+    selecionarMetodoPagamento(metodoPagamento) {
         cy.get('.input-radio').eq(2).click()
         cy.get('.woocommerce-form__input.woocommerce-form__input-checkbox.input-checkbox').click()
-        cy.get('#place_order').click()       
-        }
+        cy.get('#place_order').click()
+    }
 
-        
-        acessarPedido(pedido) {
+
+    acessarPedido(pedido) {
         cy.visit('minha-conta/pedidos/')
         cy.get('.woocommerce-order > .woocommerce-order-details > .woocommerce-order-overview').should('contain', 'Número do pedido:')
-        }
+    }
 
-        validarPedido(valPedido) {
-            cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
-            cy.get('.woocommerce-order-overview__order').should('contain', 'Número do pedido:')
-            cy.get('.woocommerce-order-overview__payment-method').should('contain', 'Método de pagamento:')     
-            cy.get('.woocommerce-order-overview__total').should('contain', 'Total')
-            cy.get('.woocommerce-column__title').should('contain', 'Endereço de faturamento')
-        } 
+    validarPedido(valPedido) {
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+        cy.get('.woocommerce-order-overview__order').should('contain', 'Número do pedido:')
+        cy.get('.woocommerce-order-overview__payment-method').should('contain', 'Método de pagamento:')
+        cy.get('.woocommerce-order-overview__total').should('contain', 'Total')
+        cy.get('.woocommerce-column__title').should('contain', 'Endereço de faturamento')
 
+    }
+
+    validandoCompra() {
+        cy.url().should('include', '/order-received/');
+        cy.contains('Obrigado. Seu pedido foi recebido.').should('be.visible');
+        cy.get('.woocommerce-order-overview__order').should('contain', 'Número do pedido:');
+        cy.get('.woocommerce-order-overview__payment-method').should('contain', 'Método de pagamento:');
+        cy.get('.woocommerce-order-overview__total').should('contain', 'Total');
+        cy.get('.woocommerce-column__title').should('contain', 'Endereço de faturamento');
+    }
 
 }
 export default new Checkout()
